44a16bb8b8f7a7baf6bb59d0c5bf8ad5fa283894
kunbr0 <toantanqn@gmail.com>
2023-09-10 06:12:04 +0300

    Setup Github actions

--- src/tests/unit/Test1.unit.test.ts
+++ src/tests/unit/Test1.unit.test.ts
@@ -1,15 +1,17 @@
 import * as lodash from 'lodash'
 
-describe('[INTEGRATION] Test1', () => {
+describe('[UNIT] Test1', () => {
   beforeAll(async () => {
     console.log('Before test')
+    console.log('process.env.TEST_VALUE: ' + process.env.TEST_VALUE)
+    console.log('Hii')
   })
 
   afterAll(async () => {
     console.log('After test')
   })
 
-  test('Test 1 - Integration', async () => {
+  test('Test 1 - Unit', async () => {
     const output = 1
     const expectedOutput = 1
 
