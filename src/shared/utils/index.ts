af09c31ebe377cd351d605bdaecc5fa92ea40c20
kunbr0 <toantanqn@gmail.com>
2024-03-01 08:31:11 +0700

    Update types and query methods for partial sensor data

--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -7,3 +7,20 @@ export function base64decode(data: string): any {
   const b = Buffer.from(data, 'base64')
   return b.toString()
 }
+
+export function getBatches(input: number, batchSize): number[] {
+  const multiples: number[] = []
+
+  const remainder = input % batchSize
+  const countOfBatches = Math.floor(input / batchSize)
+
+  for (let i = 0; i < countOfBatches; i++) {
+    multiples.push(batchSize)
+  }
+
+  if (remainder > 0) {
+    multiples.push(remainder)
+  }
+
+  return multiples
+}
