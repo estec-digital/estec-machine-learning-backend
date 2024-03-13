b0866996e3f32de70c6da3fbf0046857352ac25c
kunbr0 <toantanqn@gmail.com>
2024-03-13 13:23:47 +0200

    Add new fields to IRawSensorData interface

--- src/aws_resources/dynamodb/tables/RawSensorData/index.ts
+++ src/aws_resources/dynamodb/tables/RawSensorData/index.ts
@@ -30,6 +30,24 @@ export interface IRawSensorData {
   Ratio_PC_AVG?: number
   Result_AHC_AVG?: number
 
+  '4S1FN01DRV01_M2001_EI'?: number
+  '4K1KP01KHE01_B5001'?: number
+  '4S1GP01JST00_T8104'?: number
+  '4E1FN01TVJ01_PID_MV'?: number
+  '4E1GP01JST00_B5002'?: number
+  '4C1DD02DDJ01_M5501_MV'?: number
+  Actual_coal_PC?: number
+  CW1RB01JST00_B5001?: number
+  Actual_coal_SZ?: number
+  CW1RB02JST00_B5001?: number
+  _L72BW01_W01?: number
+  '4G1PS01GPJ02_T8201'?: number
+  '4G1PS02PGP02_T8201'?: number
+  '4G1PS03PGP02_T8201'?: number
+  Grate_Hyd_Pressure?: number
+  '_4C1BE01DRV01_M2001.Current.Value'?: number
+  '_4C1BE01DRV02_M2001.Current.Value'?: number
+
   note: {
     triggedFnProcessDataToAppDB: boolean
   }
@@ -72,7 +90,7 @@ const schemaSettings: SchemaSettings = {
   timestamps: {
     updatedAt: ['UpdatedAt'],
   },
-  saveUnknown: true
+  saveUnknown: true, // Allow any unknown fields in RawDataSensor table
 }
 
 export const RawSensorData = new DynamoDBTable<IRawSensorData, ERawSensorDataIndexes>({
